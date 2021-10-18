1e30e16b5bfd2508a00377b8b1cc93be818608b5
hungnguyen3 <hunglnl01@gmail.com>
2021-10-17 18:55:43 -0700

    add swagger & add README

---
+++ backend/Node/README.md
@@ -0,0 +1,28 @@
+# Node backend for Spotify Gen
+
+## Set-up
+
+**Requirement**
+
+- NodeJS latest v
+
+**Environment Variable**
+create a .env file in ./backend/Node folder:
+
+- PORTNUM=4000
+
+**Install**
+Run `npm install` to install all dependencies
+
+## Running local server
+
+```sh
+`npm start` or `yarn start`       # start server on 4000 w ts-node-dev
+`npm dev` or `yarn dev`           # start development on 4000 w nodemon
+`npm build` or `yarn build`       # build the server
+```
+
+## Swagger API Documentation
+
+after running `npm start`, API documatation can be accessed here:
+http://localhost:4000/api-docs/
---
+++ backend/Node/src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+const testRoute = require('../routes/testRoute');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORTNUM;
+
+// set up swagger UI
+const options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Library API',
+			version: '1.0.0',
+			descrition: 'A simple Express Library API',
+		},
+		servers: [
+			{
+				url: 'http://localhostL4000',
+			},
+		],
+	},
+	apis: ['.routes/*.js'],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
+app.get('/', (_, res) => {
+	res.send("Welcome to Spotify Gen's Node server!");
+});
+
+app.listen(port, () => {
+	console.log(`running on port ${port}`);
+});
+
+// routes
+app.use('/test', testRoute);
