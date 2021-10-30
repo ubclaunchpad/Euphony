9ca38e08705e372a591d2f02c555d54d5f2b6c92
Napat <karns.napat@gmail.com>
2021-10-30 12:28:27 -0700

    add enums for preferences

---
+++ backend/Node/src/enums.ts
@@ -0,0 +1,20 @@
+export enum Country {
+    US,
+    CA
+}
+
+export enum Activity {
+    CHILL,
+    STUDY,
+    PARTY,
+    WORK_OUT,
+    BED
+}
+
+export enum Mood {
+    HAPPY,
+    MELANCHOLY,
+    COMPASSION,
+    LONLINESS,
+    ANGER
+}
