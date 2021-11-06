409a052458652ad6266dbe71b0cf411800879edd
Fandrew44 <ahgreeting@gmail.com>
2021-11-06 12:29:29 -0700

    use single quotes for strings

--- backend/Node/routes/openWeatherRoute.ts
+++ backend/Node/routes/openWeatherRoute.ts
@@ -5,13 +5,13 @@ import { isLatitude, isLongitude } from './route-helpers/mapbox-helpers';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Open Weather API");
+    res.send('Open Weather API');
 });
 
 router.get('/reverseWeather/:latLon', async (req, res) => {
     const latLon = req.params.latLon.split(',');
     if (!isLatitude(latLon[0]) || !isLongitude(latLon[1])) {
-        res.status(400).send("Invalid lat/lon value(s)");
+        res.status(400).send('Invalid lat/lon value(s)');
     }
     const weatherData = await currentWeatherData(latLon[0], latLon[1]);
 
