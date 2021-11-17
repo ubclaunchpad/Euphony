7abaacc051ca25b75f76682dccc95c8899b6ccce
hungng <hung.nguyen@git.contigo.lan>
2021-11-17 13:15:48 -0800

    documentation starter

--- backend/Node/src/index.ts
+++ backend/Node/src/index.ts
@@ -1,43 +1,36 @@
 import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
-import bodyParser from 'body-parser';
 const testRoute = require('../routes/testRoute');
 const mapboxRoute = require('../routes/mapboxRoute');
 const spotifyRoute = require('../routes/spotifyRoute');
 
 require('dotenv').config();
 
-const app = express();
 const port = process.env.PORTNUM;
-
-// support parsing of application/json type post data
-app.use(bodyParser.json());
-
-//support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
 
 // set up swagger UI
 const options = {
 	definition: {
-		openapi: '3.0.0',
+		openapi: "3.0.0",
 		info: {
-			title: 'Library API',
-			version: '1.0.0',
-			descrition: 'A simple Express Library API',
+			title: "Spotify Gen API Documentation",
+			version: "1.0.0",
+			description: "Spotify Gen Library API",
 		},
 		servers: [
 			{
-				url: 'http://localhostL4000',
+				url: "http://localhost:4000",
 			},
 		],
 	},
-	apis: ['.routes/*.js'],
+	apis: ["./routes/docs/*.yaml"],
 };
 
 const specs = swaggerJsDoc(options);
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.get('/', (_, res) => {
 	res.send("Welcome to Spotify Gen's Node server!");
