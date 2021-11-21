e3b46dcf6b056718c110f735c83109ba3a462531
Collin Eng <eng.engineereng@gmail.com>
2021-11-21 01:28:31 -0800

    SG-36 refined carousel, added header, styling

---
+++ frontend/components/filter/Carousel/styles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10
+    },
+    title: {
+        color: 'hsl(0, 0%, 15%)',
+        fontSize: 30,
+        fontWeight: 'bold'
+    }
+});
+
+export default styles;
---
+++ frontend/types.tsx
@@ -0,0 +1,4 @@
+export type Choice = {
+    id: string;
+    label: string; //TODO add API filter value
+};
