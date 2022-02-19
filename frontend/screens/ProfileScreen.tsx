52ba99f2e399ead1d0db0d4acd94fa56c32179ef
Voctor <voctorbidoof@gmail.com>
2022-02-19 12:15:45 -0800

    Add base Profile Screen to navigation

---
+++ frontend/screens/ProfileScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar, SafeAreaView  } from 'react-native';
+
+const ProfileScreen = () => {
+   
+
+    return (
+        <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
+            <StatusBar barStyle="light-content" backgroundColor="white" />
+
+        </SafeAreaView>
+    );
+};
+
+export default ProfileScreen;
