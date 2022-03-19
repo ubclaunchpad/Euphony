552c1fa87dd781c29cca440c6b61ed11222b0216
Napat <karns.napat@gmail.com>
2022-03-19 05:13:15 -0700

    added sf into city weather

--- backend/Node/src/db/cityWeather.ts
+++ backend/Node/src/db/cityWeather.ts
@@ -13,6 +13,7 @@ const latlonOfBigCities: CityWeather[] = [
 	{ cityName: 'Toronto', latlon: ['43.761539', '-79.411079'] },
 	{ cityName: 'Burnaby', latlon: ['49.246445', '-122.994560'] },
 	{ cityName: 'Richmond', latlon: ['49.166592', '-123.133568'] },
+	{ cityName: 'San Francisco', latlon: ['37.7749', '-122.4194']}
 ];
 
 export const fetchWeatherData = async (hoursBetweenFetch: number) => {
